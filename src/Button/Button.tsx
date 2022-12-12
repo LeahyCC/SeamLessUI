a838513a3b8066d091bf31d6709c1c08f026cb00
Colin Leahy <ccleahy00@hotmail.com>
2022-12-12 09:39:14 -0700

    storybook

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,16 +1,48 @@
-import { ReactNode } from 'react'
+import React from 'react';
+import './button.css';
 
-type ButtonProps = {
-  children: ReactNode
-  onClick: () => void
-  className?: string
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
 }
 
-export const Button = (props: ButtonProps) => {
-  const { children, onClick, className, ...rest } = props
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button className={className} onClick={onClick} {...rest}>
-      {children}
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
     </button>
-  )
-}
+  );
+};
